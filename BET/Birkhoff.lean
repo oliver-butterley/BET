77df3dca116c654531be588adeeb7951540aab37
Oliver Butterley <oliver.butterley@gmail.com>
2024-03-01 20:37:51 +0100

    ✨ rough version of div_set and div_set_inv

--- BET/Birkhoff.lean
+++ BET/Birkhoff.lean
@@ -22,29 +22,62 @@ section Ergodic_Theory
 
 open BigOperators MeasureTheory
 
-
 /-
 - `T` is a measure preserving map of a probability space `(α, μ)`,
 - `f g : α → ℝ` are integrable.
 -/
 variable {α : Type*} [MeasurableSpace α]
 variable {μ : MeasureTheory.Measure α} [MeasureTheory.IsProbabilityMeasure μ]
-variable {T : α → α} (hT : MeasurePreserving T μ)
-variable {f g : α → ℝ} (hf : Integrable f μ) (hg : Integrable g μ)
-variable (x : α)
-variable (n : ℕ)
+variable (T : α → α) (hT : MeasurePreserving T μ)
+variable (f g : α → ℝ) (hf : Integrable f μ) (hg : Integrable g μ)
 
-/- define `A := { x | sup_n ∑_{i=0}^{n} f(T^i x) = ∞}`.
-def A := { x : α | ∃ n, ∀ C : ℝ, ∑ i in Finset.range n, f (T^[i] x) > C }
 
-define `A := { x | lim_n ∑_{i=0}^{n} f(T^i x) = ∞}`.
--/
-def A := { x : α | Filter.Tendsto (fun n ↦ ∑ i in Finset.range n, f (T^[i] x)) Filter.atTop Filter.atTop }
+/-- The set of divergent points `{ x | sup_n ∑_{i=0}^{n} f(T^i x) = ∞}`. -/
+def div_set := { x : α |  ∀ C : ℝ, ∃ n, C < birkhoffSum T f n x }
 
+theorem div_set_inv_aux (x : α) (hx : x ∈ div_set T f) :
+    ∀ C : ℝ, ∃ n, 1 ≤ n ∧ C < birkhoffSum T f n x := sorry
+
+/-- The set of divergent points is invariant. -/
+theorem div_set_inv : T⁻¹' (div_set T f) = (div_set T f) := by
+  unfold div_set
+  ext x
+  have h1 (n : ℕ) (x : α) :
+      birkhoffSum T f n (T x) = birkhoffSum T f (n + 1) x - f x := by
+    rw [birkhoffSum_add T f n 1 x]
+    rw [eq_add_of_sub_eq' (birkhoffSum_apply_sub_birkhoffSum T f n x)]
+    simp
+    exact add_sub (birkhoffSum T f n x) (f (T^[n] x)) (f x)
+  constructor
+  intro h0
+  intro C
+  have h2 := h0 (C - (f x))
+  obtain ⟨n, h3⟩ := h2
+  rw [h1 n x] at h3
+  use (n + 1)
+  simp at h3
+  exact h3
+  intro h4
+  simp
+  simp at h4
+  intro C
+  let C' := C
+  have h5 := h4 (C + (f x))
+  obtain ⟨n, h6⟩ := h5
+  have h7 : 1 ≤ n := sorry
+  use n - 1
+  rw [h1]
+  have h8 : n - 1 + 1 = n := Nat.sub_add_cancel h7
+  rw [h8]
+  exact lt_tsub_iff_right.mpr h6
+
+-- def A := { x : α | Filter.Tendsto (fun n ↦ ∑ i in Finset.range n, f (T^[i] x)) Filter.atTop Filter.atTop }
 /- `A` is in `I = inv_sigma_algebra`. -/
 -- idea: is it better to define a new type measureable sets in alpha and then restrict to that type?
 -- def inv_sigma_algebra := { S : Set α | MeasurableSet S ∧ T⁻¹' S = S }
 def inv_sigma_algebra := { S : Set α | MeasurableSet S ∧ IsInvariant (fun n x ↦ T^[n] x) S }
+-- `IsInvariant` is defined as: a set s ⊆ α is invariant under ϕ : τ → α → α if ϕ t s ⊆ s for all t in τ.
+
 
 /- define `Φ_n : max { ∑_{i=0}^{n} φ ∘ T^i }_{k ≤ n}` -/
 
@@ -71,7 +104,7 @@ then `μ(A) = 0`. -/
 
 /- Birkhoff's theorem: Almost surely, `birkhoffAverage ℝ f g n x` converges to the conditional expectation of `f`. -/
 
-#check birkhoffAverage ℝ T f n x
+-- #check birkhoffAverage ℝ T f n x
 
 /- If `T` is ergodic, show that the invariant sigma-algebra is a.e. trivial. -/
 
